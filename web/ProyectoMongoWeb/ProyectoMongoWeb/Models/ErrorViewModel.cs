7ffd895c1fa391e82d9b4122e0c8e4f1bcc58873
Luis David <luisdavidmirandav12@gmail.com>
2025-03-13 08:57:33 -0600

    proyecto .net core 8 inicializado
    Aplicacion web instalada

---
+++ web/ProyectoMongoWeb/ProyectoMongoWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoMongoWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
